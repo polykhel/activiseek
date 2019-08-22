b08f7b93e647848c55ce04ba5c722cebaf4bbd5c
Michael Pollente <pollentem@gmail.com>
2019-08-22 17:16:45 +0800

    Remove IntegrationConfiguration

--- src/main/java/com/activiseek/api/config/IntegrationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.activiseek.api.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.config.EnableIntegration;
-
-@Configuration
-@EnableIntegration
-public class IntegrationConfiguration {
-}
